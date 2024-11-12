726f37f6982ae11feea298b412a5c860bc33da0b
Vinícius Medrano <78562910+medranogit@users.noreply.github.com>
2024-11-12 00:22:07 -0300

    Hosting_project

--- store_site/store_site/settings.py
+++ store_site/store_site/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -76,11 +77,17 @@ WSGI_APPLICATION = 'store_site.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(
+        os.environ.get("DATABASE_URL", "postgresql://cupcake_db_user:M1jgPkKsrfLgLhSK0feKMaYQeErZzFST@dpg-cspc108gph6c73d02jig-a.oregon-postgres.render.com/cupcake_db")
+    )
 }
 
 
--- store_site/store_site/urls.py
+++ store_site/store_site/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 from django.contrib.auth import views as auth_views  # Importa views de autenticação padrão
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 
 urlpatterns = [
@@ -31,3 +32,5 @@ urlpatterns = [
 # Adicione esta linha para lidar com arquivos de mídia no desenvolvimento
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += staticfiles_urlpatterns()
