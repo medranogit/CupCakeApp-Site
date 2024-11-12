947ff92abc3cd215ccf81f21976c0a7e0c1ff5f4
Vinícius Medrano <78562910+medranogit@users.noreply.github.com>
2024-11-12 02:14:48 -0300

    remove_wsgi

--- store_site/settings.py
+++ store_site/settings.py
@@ -14,8 +14,6 @@ from pathlib import Path
 import os
 import dj_database_url
 from django.contrib.auth.models import User
-import django
-
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
--- store_site/wsgi.py
+++ store_site/wsgi.py
@@ -8,23 +8,8 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
 
 import os
-import django
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'store_site.settings')
 
 application = get_wsgi_application()
-
-# Código para criar automaticamente o superusuário
-from django.contrib.auth import get_user_model
-
-User = get_user_model()
-username = os.environ.get("DJANGO_SUPERUSER_USERNAME", "admin")
-email = os.environ.get("DJANGO_SUPERUSER_EMAIL", "admin@example.com")
-password = os.environ.get("DJANGO_SUPERUSER_PASSWORD", "admin@vini312")
-
-if not User.objects.filter(username=username).exists():
-    User.objects.create_superuser(username=username, email=email, password=password)
-    print(f'Superusuário "{username}" criado com sucesso!')
-else:
-    print(f'Superusuário "{username}" já existe.')
